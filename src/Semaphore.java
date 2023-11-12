befd82fd658638413626e1e872e6e0b061b5c9d5
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2023-11-12 20:56:28 +0200

    done

--- src/Semaphore.java
+++ src/Semaphore.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
@@ -18,10 +19,12 @@ enum DeviceType {
     Mobile, PC, Tablet
 }
 
-class Device implements Runnable {
+class Device extends Thread {
     private String name;
     private DeviceType type;
     private Router router;
+    private int index;
+    Semaphore semaphore;
 
     public Device(String name, DeviceType type, Router router) {
         this.router = router;
@@ -31,21 +34,23 @@ class Device implements Runnable {
 
     @Override
     public void run() {
-        router.connect();
 
+        router.addDevice(this);
+        System.out.println("before connection " + name + " at index " + index);
+        //router.connect();
+        System.out.println("after connection " + name + " at index " + index);
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+
+        }
         // Wait random time.
         // Do some activity.
         // Wait random time.
-
+        router.resetConnection(index);
+        System.out.println("after release " + name + " at index " + index);
         router.release();
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public DeviceType getType() {
@@ -56,24 +61,56 @@ class Device implements Runnable {
         this.type = type;
     }
 
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
     public void logout() {
 
     }
 }
 
 class Router {
-    List<Device> devices;
-    Semaphore semaphore = new Semaphore(5);
+    Device[] devices = new Device[2];
+    Semaphore semaphore = new Semaphore(2);
+
+    int idx = 0;
 
     public Router() {
+        for (int i = 0; i < 2; i++) {
+            devices[i] = null;
+        }
+    }
+
+    public void addDevice(Device d) {
+        try {
+
+            semaphore.acquire();
+        } catch (Exception e) {
+
+        }
+        for (int i = 0; i < 2; i++) {
+            if (devices[i] == null) {
+                devices[i] = d;
+                d.setIndex(i);
+                break;
+            }
+        }
+
+    }
+
+    public void resetConnection(int i) {
+        devices[i] = null;
     }
 
     public void connect() {
         try {
+            for (Device d : devices) {
+                d.start();
+            }
             semaphore.acquire();
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
 
         }
     }
@@ -81,22 +118,23 @@ class Router {
     public void release() {
         try {
             semaphore.release();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
 
         }
     }
 }
 
 class Network {
-    static void main() {
+    public static void main(String args[]) {
         Router router = new Router();
 
         Device d1 = new Device("d1", DeviceType.PC, router);
+        Device d2 = new Device("d2", DeviceType.Mobile, router);
+        Device d3 = new Device("d3", DeviceType.PC, router);
+        d1.start();
+        d2.start();
+        d3.start();
 
-        d1.run();
-
-        List<Device> devices;
 
     }
 }
